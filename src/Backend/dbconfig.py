eafe1d9f567d0e7412a979e20beb758ec056d917
Samarth <samarthp1@gmail.com>
2022-12-05 14:38:19 -0500

    Removed mock module from python tests

--- src/Backend/dbconfig.py
+++ src/Backend/dbconfig.py
@@ -1,10 +1,21 @@
 from asyncio import constants
+import sys
 
 # Constant Variables
-constants = {
-    "host": "127.0.0.1",
-	"port": 8888,
-    "user": "root",
-    "password": "password",
-    "database": "donationsystem"
-}
+if 'win' in sys.platform:
+	constants = {
+		"host": "localhost",
+		"port": 3306,
+		"user": "root",
+		"password": "",
+		"database": "donationsystem"
+	}
+
+else:
+	constants = {
+		"host": "127.0.0.1",
+		"port": 8888,
+		"user": "root",
+		"password": "password",
+		"database": "donationsystem"
+	}
